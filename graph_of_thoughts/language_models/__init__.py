3fb3bcd80fea0e947895d7be5a7add6c648e9109
PengXinyang <148336099@qq.com>
2026-03-29 18:25:04 +0800

    fix: 删除了llama

--- graph_of_thoughts/language_models/__init__.py
+++ graph_of_thoughts/language_models/__init__.py
@@ -13,7 +13,6 @@
 
 from .abstract_language_model import AbstractLanguageModel
 from .chatgpt import ChatGPT
-from .llamachat_hf import Llama2HF
 from .deepseek import DeepSeek
 from .gemini import Gemini
 from .gcli_gemini import GCLIGemini
